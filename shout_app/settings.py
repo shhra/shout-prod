3e8dab06f54984782a5ce018eb027c7c38718247
shhra <071bct538@pcampus.edu.np>
2019-09-16 23:43:22 +0545

    Trying prod -21

--- shout_app/settings.py
+++ shout_app/settings.py
@@ -157,8 +157,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'), )
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'), )
 
 # Media
 MEDIA_URL = '/mediafiles/'
