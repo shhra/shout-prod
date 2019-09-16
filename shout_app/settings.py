271b3d2bd7a948de27931ff26df48a1b1e2ea3cf
shhra <071bct538@pcampus.edu.np>
2019-09-16 18:38:56 +0545

    Trying prod -2

--- shout_app/settings.py
+++ shout_app/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
+django_heroku.settings(locals)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
