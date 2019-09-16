80e69c7731d44a8e545fa7e3f518cebeb25c41f7
shhra <071bct538@pcampus.edu.np>
2019-09-16 18:56:05 +0545

    Trying prod -5

--- shout_app/settings.py
+++ shout_app/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-PROJ_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -156,6 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'), )
 
 # Media
