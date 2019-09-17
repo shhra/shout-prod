ddecf4e807a3480dfda91afb9aa2497a10634147
shhra <071bct538@pcampus.edu.np>
2019-09-17 07:18:01 +0545

    Trying prod -23

--- master/urls.py
+++ master/urls.py
@@ -28,19 +28,19 @@ urlpatterns = [
     path('c/<slug:slug>/echoed', EchoList.as_view(), name='echo_shout'),
 
     # api urls
-    path('api/', APIRoot.as_view(), name='api_root'),
-    path('api/s/', ShoutListAPI.as_view(), name='shouts_api'),
-    path('api/s/create/', CreateShoutAPI.as_view(), name='create_shout_api'),
-    path('api/s/<slug:slug>/', ShoutDetailAPI.as_view(), name='shout_detail_api'),
-    path('api/s/<slug:slug>/support/', SupportShoutAPI.as_view(), name='support_shout_api'),
+    # path('api/', APIRoot.as_view(), name='api_root'),
+    # path('api/s/', ShoutListAPI.as_view(), name='shouts_api'),
+    # path('api/s/create/', CreateShoutAPI.as_view(), name='create_shout_api'),
+    # path('api/s/<slug:slug>/', ShoutDetailAPI.as_view(), name='shout_detail_api'),
+    # path('api/s/<slug:slug>/support/', SupportShoutAPI.as_view(), name='support_shout_api'),
 
-    path('api/user/', UserListAPI.as_view(), name='user_list_api'),
-    path('api/user/<str:username>/', UserDetailAPI.as_view(), name='customuser-detail'),
-    path('api/signup', SignupAPI.as_view(), name='user_signup_api'),
+    # path('api/user/', UserListAPI.as_view(), name='user_list_api'),
+    # path('api/user/<str:username>/', UserDetailAPI.as_view(), name='customuser-detail'),
+    # path('api/signup', SignupAPI.as_view(), name='user_signup_api'),
 
-    path('api/c/', CommentListAPI.as_view(), name='comment_api'),
-    path('api/c/create/', CreateCommentAPI.as_view(), name='create_comment_api'),
-    path('api/c/<slug:slug>/', CommentDetailAPI.as_view(), name='comment_detail_api'),
+    # path('api/c/', CommentListAPI.as_view(), name='comment_api'),
+    # path('api/c/create/', CreateCommentAPI.as_view(), name='create_comment_api'),
+    # path('api/c/<slug:slug>/', CommentDetailAPI.as_view(), name='comment_detail_api'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
